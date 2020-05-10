de8a40f82498d1ae20ef0af707e912f5fd6c1883
Amir <55581295+amirzubi@users.noreply.github.com>
2020-05-10 16:55:42 +0200

    Create display_product_price.php

---
+++ shortcodes/display_product_price.php
@@ -0,0 +1,19 @@
+function displayProductPrice( $atts ) {
+	$atts = shortcode_atts( array(
+		'id' => null
+	), $atts, 'product_price' );
+ 
+	if ( empty( $atts[ 'id' ] ) ) {
+		return '';
+	}
+ 
+	$product = wc_get_product( $atts['id'] );
+ 
+	if ( ! $product ) {
+		return '';
+	}
+ 
+	return $product->get_price_html();
+}
+ 
+add_shortcode( 'product_price', 'displayProductPrice' );
